1a76d2b58f86aa8e44e629e1e7d18e2be20c6124
Joseph Zurowski <Kexerval@users.noreply.github.com>
2019-09-24 19:09:42 -0400

    Convert to Pug

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,19 @@
-<template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
+<template lang="pug">
+  v-app
+    v-app-bar(app)
+      v-toolbar-title.headline.text-uppercase
+        span Vuetify
+        span.font-weight-light MATERIAL DESIGN
+      v-spacer
+      v-btn(
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>fas fa-external-link-alt</v-icon>
-      </v-btn>
-    </v-app-bar>
+        text
+      )
+        span.mr-2 Latest Release
+        v-icon fas fa-external-link-alt
 
-    <v-content>
-      <router-view/>
-    </v-content>
-  </v-app>
+    v-content: router-view
 </template>
 
 <script>
