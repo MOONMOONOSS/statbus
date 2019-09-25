b946f28c63b47fc2d1e05bb9adaac4ea6ee50b9e
Joseph Zurowski <Kexerval@users.noreply.github.com>
2019-09-25 15:53:00 -0400

    Add data table
    Guess that pagination was for nothing lol

--- src/views/RoundOverview.vue
+++ src/views/RoundOverview.vue
@@ -1,23 +1,96 @@
 <template lang="pug">
-  v-container.mx-sm-12(fill-height)
+  v-container(fill-height)
     v-layout(column)
-      v-col.shrink
-        .display-2.d-none.d-sm-block Round Listings
-        .display-1.d-block.d-sm-none.text-center Round Listings
-        v-divider.my-6.d-none.d-sm-block
-      v-spacer
       v-col.text-center.shrink
-        v-pagination(
-          v-model="page"
-          :length="15"
-          :total-visible="7"
+        v-data-table.elevation-1(
+          dense
+          :headers="headers"
+          :items="rounds"
+          :items-per-page="25"
+          :search="search"
+          item-key="id"
         )
+          template(v-slot:top)
+            v-toolbar(
+              flat
+              color="white"
+            )
+              v-toolbar-title Round Listings
+              v-divider.mx-4(
+                inset
+                vertical
+              )
+              v-spacer
+              v-toolbar-items.d-none.d-sm-flex
+                v-text-field(
+                  clearable
+                  flat
+                  solo
+                  v-model="search"
+                  prepend-icon="fa-search"
+                  label="Search"
+                )
+            v-text-field.d-flex.d-sm-none.mx-4(
+              clearable
+              flat
+              solo
+              v-model="search"
+              prepend-icon="fa-search"
+              label="Search"
+              style="max-height: 48px"
+            )
 </template>
 
 <script>
 export default {
   data: () => ({
-    page: 1,
+    headers: [
+      {
+        text: 'ID',
+        value: 'id',
+      },
+      {
+        text: 'Mode',
+        value: 'mode',
+      },
+      {
+        text: 'Result',
+        value: 'result',
+      },
+      {
+        text: 'Map',
+        value: 'map',
+      },
+      {
+        text: 'Start',
+        value: 'startTime',
+      },
+      {
+        text: 'Duration',
+        value: 'duration',
+      },
+      {
+        text: 'End',
+        value: 'endTime',
+      },
+      {
+        text: 'Server',
+        value: 'server',
+      },
+    ],
+    rounds: [
+      {
+        id: 162,
+        mode: 'Traitor',
+        result: 'Proper Completion',
+        map: 'PubbyStation',
+        startTime: '2019-09-25 04:21:24',
+        duration: '08:53:40',
+        endTime: '2019-09-25 13:10:52',
+        server: 'MoonStation',
+      },
+    ],
+    search: '',
   }),
 };
 </script>
