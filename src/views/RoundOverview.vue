8da5d148db480966a572350045ef57cf7dd58714
Joseph Zurowski <Kexerval@users.noreply.github.com>
2019-09-25 14:53:53 -0400

    Add pagination bar

--- src/views/RoundOverview.vue
+++ src/views/RoundOverview.vue
@@ -1,10 +1,23 @@
 <template lang="pug">
   v-container.mx-sm-12(fill-height)
-    p Placeholder
+    v-layout(column)
+      v-col.shrink
+        .display-2.d-none.d-sm-block Round Listings
+        .display-1.d-block.d-sm-none.text-center Round Listings
+        v-divider.my-6.d-none.d-sm-block
+      v-spacer
+      v-col.text-center.shrink
+        v-pagination(
+          v-model="page"
+          :length="15"
+          :total-visible="7"
+        )
 </template>
 
 <script>
 export default {
-  //
+  data: () => ({
+    page: 1,
+  }),
 };
 </script>
